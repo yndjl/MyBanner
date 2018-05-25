7373b75cff4eee6c8e184318383a3d9c9314eedf
与你的距离 <guolima1233@163.com>
2018-05-25 18:40:49 +0800

    优化控件的参数设置功能

--- app/src/main/java/com/mgl/mybanner/MainActivity.java
+++ app/src/main/java/com/mgl/mybanner/MainActivity.java
@@ -1,15 +1,10 @@
 package com.mgl.mybanner;
 
 import android.os.Bundle;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.mgl.mybannerview.MyBannerViewImageView;
 import com.mgl.mybannerview.MyBannerViewViewPager;
 
 import java.util.ArrayList;
@@ -31,24 +26,39 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initBannerByViewPager() {
         MyBannerViewViewPager viewViewPager = (MyBannerViewViewPager) findViewById(R.id.bannerVP);
-        ImageView imageView0 = new ImageView(this);
-        imageView0.setImageResource(com.mgl.mybannerview.R.drawable.shouye1);
-//        imageView0.setImageResource(com.mgl.mybannerview.R.drawable.a1);
-        ImageView imageView1 = new ImageView(this);
-        imageView1.setImageResource(com.mgl.mybannerview.R.drawable.shouye3);
-//        imageView1.setImageResource(com.mgl.mybannerview.R.drawable.a2);
-        ImageView imageView2 = new ImageView(this);
-        imageView2.setImageResource(com.mgl.mybannerview.R.drawable.shouye4);
-//        imageView2.setImageResource(com.mgl.mybannerview.R.drawable.a3);
-        ImageView imageView3 = new ImageView(this);
-        imageView3.setImageResource(com.mgl.mybannerview.R.drawable.shouye5);
-//        imageView3.setImageResource(com.mgl.mybannerview.R.drawable.a1);
-        data.add(imageView0);
-        data.add(imageView1);
-        data.add(imageView2);
-        data.add(imageView3);
-        MyBannerViewViewPager.Builder builder=viewViewPager.new Builder(this,data);
-        builder.setPageScaleMin(0.65f).setViewPaddingLeftAndRight(50).build();
+//        ImageView imageView0 = new ImageView(this);
+//        imageView0.setImageResource(com.mgl.mybannerview.R.drawable.shouye1);
+////        imageView0.setImageResource(com.mgl.mybannerview.R.drawable.a1);
+//        ImageView imageView1 = new ImageView(this);
+//        imageView1.setImageResource(com.mgl.mybannerview.R.drawable.shouye3);
+////        imageView1.setImageResource(com.mgl.mybannerview.R.drawable.a2);
+//        ImageView imageView2 = new ImageView(this);
+//        imageView2.setImageResource(com.mgl.mybannerview.R.drawable.shouye4);
+////        imageView2.setImageResource(com.mgl.mybannerview.R.drawable.a3);
+//        ImageView imageView3 = new ImageView(this);
+//        imageView3.setImageResource(com.mgl.mybannerview.R.drawable.shouye5);
+////        imageView3.setImageResource(com.mgl.mybannerview.R.drawable.a1);
+//
+//
+//        ImageView imageView4 = new ImageView(this);
+//        imageView4.setImageResource(com.mgl.mybannerview.R.drawable.shouye1);
+//        ImageView imageView5 = new ImageView(this);
+//        imageView5.setImageResource(com.mgl.mybannerview.R.drawable.shouye5);
+//        data.add(imageView5);
+//        data.add(imageView0);
+//        data.add(imageView1);
+//        data.add(imageView2);
+//        data.add(imageView3);
+//        data.add(imageView4);
+        int[] data=new int[]{R.drawable.shouye1,R.drawable.shouye3,R.drawable.shouye4,R.drawable.shouye5};
+        MyBannerViewViewPager.Builder builder=viewViewPager.new Builder(this,data,MyBannerViewViewPager.BANNER_TYPE_MEIZU);
+        builder.setPageScaleMin(0.65f)
+                .setViewPaddingLeftAndRight(50)
+                .setDelayMillis(4500)
+                .setVviewPagerCurrentItem(0)
+                .setViewPagerMarginLeftAndRight(150)
+                .setOpenCarousel(true)
+                .build();
     }
 
     /**
--- mybannerview/src/main/java/com/mgl/mybannerview/MyBannerViewViewPager.java
+++ mybannerview/src/main/java/com/mgl/mybannerview/MyBannerViewViewPager.java
@@ -1,17 +1,20 @@
 package com.mgl.mybannerview;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,19 +23,34 @@ import java.util.List;
  */
 
 public class MyBannerViewViewPager extends RelativeLayout {
+    public static final int BANNER_TYPE_MEIZU = 1;//开启魅族模式
+    public static final int BANNER_TYPE_NOMOL = 2;//普通模式
     private ViewPager viewPager;
     private Context context;
     private AdapterViewPager adapter;
-    private List<View> data;
-    int viewPaddingLeftAndRight = 30;//viewPager中控件左右padding距离
+    private List<View> data = new ArrayList<>();//view数据
+    int childViewPaddingLeftAndRight = 30;//viewPager中控件左右padding距离
     float pageScaleMin = 0.8f;//页面缩放比列
+    int viewPagerMarginLeftAndRight = 150;//设置viewPager距离父控件的margin距离
+    int bannerType = BANNER_TYPE_NOMOL;
+    LayoutParams viewPagerLayoutParams;
+    int delayMillis = 4000;//轮播的延时时间
+    int viewPagerCurrentItem = 0;//初始化轮播显示View的位置
+    private Boolean openCarousel = true;//轮播模式
+    int[] dataRes;//图片来源---资源ID
+    String[] dataUrl;//图片来源---网络url地址
 
     public void initMyBannerViewValue(Builder builder) {
         this.context = builder.context;
-        this.data = builder.data;
-        this.viewPaddingLeftAndRight = builder.viewPaddingLeftAndRight;
+        this.dataRes = builder.dataRes;
+        this.dataUrl = builder.dataUrl;
+        this.childViewPaddingLeftAndRight = builder.childViewPaddingLeftAndRight;
         this.pageScaleMin = builder.pageScaleMin;
-
+        this.viewPagerMarginLeftAndRight = builder.viewPagerMarginLeftAndRight;
+        this.bannerType = builder.bannerType;
+        this.delayMillis = builder.delayMillis;
+        this.viewPagerCurrentItem = builder.viewPagerCurrentItem;
+        this.openCarousel = builder.openCarousel;
         startViewPager();
     }
 
@@ -45,6 +63,11 @@ public class MyBannerViewViewPager extends RelativeLayout {
         this.context = context;
         View inflate = LayoutInflater.from(context).inflate(R.layout.my_banner_view_vp, this, true);
         viewPager = (ViewPager) inflate.findViewById(R.id.vp_my_banner_view_vp);
+        if (bannerType == 1) {//xml中设置为魅族模式时，设置viewPager的margin距离
+            viewPagerLayoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            viewPagerLayoutParams.setMargins(150, 0, 150, 0);
+            viewPager.setLayoutParams(viewPagerLayoutParams);
+        }
     }
 
 
@@ -52,16 +75,96 @@ public class MyBannerViewViewPager extends RelativeLayout {
      * 初始化ViewPager页面
      */
     public void startViewPager() {
-        for (int i = 0; i < data.size(); i++) {
-            View e = data.get(i);
-            e.setPadding(viewPaddingLeftAndRight, 0, viewPaddingLeftAndRight, 0);
+        if (dataRes != null) {//使用apk图片资源id来初始化data数据
+            for (int i = 0; i < dataRes.length + 2; i++) {//设置viewPager中page的左右padding，使效果中两个页面间可以留白
+                ImageView imageView = new ImageView(context);
+                if (i == 0) {
+                    imageView.setImageResource(dataRes[dataRes.length - 1]);
+                    data.add(imageView);
+                } else if (i == dataRes.length + 1) {
+                    imageView.setImageResource(dataRes[0]);
+                    data.add(imageView);
+                } else {
+                    imageView.setImageResource(dataRes[i - 1]);
+                    data.add(imageView);
+                }
+                imageView.setPadding(childViewPaddingLeftAndRight, 0, childViewPaddingLeftAndRight, 0);
+            }
         }
+        if (dataUrl != null) {
+
+        }
+
+
+        if (bannerType == 1) {//设置为魅族模式时，设置viewPager的margin距离,以便开启魅族样式
+            viewPagerLayoutParams = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            viewPagerLayoutParams.setMargins(viewPagerMarginLeftAndRight, 0, viewPagerMarginLeftAndRight, 0);
+            viewPager.setLayoutParams(viewPagerLayoutParams);
+        }
+
+
         adapter = new AdapterViewPager(this.data);
         viewPager.setOffscreenPageLimit(2);
         viewPager.setAdapter(adapter);
+        viewPager.setCurrentItem(viewPagerCurrentItem + 1);//设置为魅族模式时，设置初始page时，页面页面在data中的position加1
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (positionOffset != 0.0f) {
+                    handler.removeCallbacksAndMessages(null);
+                } else {
+                    handler.postDelayed(target, delayMillis);
+                }
+
+                // TODO: 2018/5/25 滑动的时候的页面计算
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                viewPagerCurrentItem = position;
+                Log.d("MyBannerView", "viewPagerCurrentItem    " + viewPagerCurrentItem);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
 
         setImgOutScreenScaleAnimation();
+        if (openCarousel)
+            handler.postDelayed(target, delayMillis);
     }
+
+    /**
+     * 循环线程初始化
+     */
+    Runnable target = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("MyBannerView", "target");
+            handler.obtainMessage().sendToTarget();
+            handler.postDelayed(target, delayMillis);
+        }
+    };
+
+    /**
+     * viewPager页面更新
+     */
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (viewPagerCurrentItem + 1 < data.size() - 1) {
+                viewPagerCurrentItem++;
+                viewPager.setCurrentItem(viewPagerCurrentItem);
+            } else if (viewPagerCurrentItem + 1 == data.size() - 1) {
+                viewPagerCurrentItem = 1;
+                viewPager.setCurrentItem(viewPagerCurrentItem);
+            }
+
+        }
+    };
 
 
     /**
@@ -82,24 +185,59 @@ public class MyBannerViewViewPager extends RelativeLayout {
         viewPager.setPageTransformer(false, pageTransformer);
     }
 
+
     public class Builder {
         Context context;
-        List<View> data;
-        int viewPaddingLeftAndRight = 30;
+        int childViewPaddingLeftAndRight = 30;
         float pageScaleMin = 0.8f;
+        int viewPagerMarginLeftAndRight = 150;
+        int bannerType = BANNER_TYPE_NOMOL;
+        int delayMillis = 4000;
+        int viewPagerCurrentItem = 0;
+        private Boolean openCarousel = true;//轮播模式
+        int[] dataRes;
+        String[] dataUrl;
 
-        public Builder(Context context, List<View> data) {
+
+        public Builder(Context context, int[] dataRes, int bannerType) {
             this.context = context;
-            this.data = data;
+            this.dataRes = dataRes;
+            this.bannerType = bannerType;
         }
 
-        public Builder setViewPaddingLeftAndRight(int viewPaddingLeftAndRight) {
-            this.viewPaddingLeftAndRight = viewPaddingLeftAndRight;
+        public Builder(Context context, String[] dataUrl, int bannerType) {
+            this.context = context;
+            this.dataUrl = dataUrl;
+            this.bannerType = bannerType;
+        }
+
+        public Builder setViewPaddingLeftAndRight(int childViewPaddingLeftAndRight) {
+            this.childViewPaddingLeftAndRight = childViewPaddingLeftAndRight;
             return this;
         }
 
         public Builder setPageScaleMin(float pageScaleMin) {
             this.pageScaleMin = pageScaleMin;
+            return this;
+        }
+
+        public Builder setViewPagerMarginLeftAndRight(int viewPagerMarginLeftAndRight) {
+            this.viewPagerMarginLeftAndRight = viewPagerMarginLeftAndRight;
+            return this;
+        }
+
+        public Builder setDelayMillis(int delayMillis) {
+            this.delayMillis = delayMillis;
+            return this;
+        }
+
+        public Builder setVviewPagerCurrentItem(int viewPagerCurrentItem) {
+            this.viewPagerCurrentItem = viewPagerCurrentItem;
+            return this;
+        }
+
+        public Builder setOpenCarousel(boolean openCarousel) {
+            this.openCarousel = openCarousel;
             return this;
         }
 
